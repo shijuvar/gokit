d4ad47b1637bb527e934ec55b6f08a290f76d241
Shiju Varghese <shiju.varghese@outlook.com>
2017-03-27 15:35:39 +0530

    Example for error with package github.com/pkg/errors

---
+++ examples/error/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// ReadFile read the given file
+func ReadFile(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.WithMessage(err, "Failed to open file")
+	}
+	defer f.Close()
+
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to read the file")
+	}
+	return buf, nil
+}
+
+func main() {
+	data, err := ReadFile("data.txt")
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Print(string(data))
+}
