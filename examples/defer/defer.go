ad591a0a9141db159779bd7b4588972ea5515e75
Shiju Varghese <shiju.varghese@outlook.com>
2017-04-04 18:28:59 +0530

    minor modifications

---
+++ examples/defer/defer.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	defer fmt.Println("deferred function 1, value of is", i)
+	defer fmt.Println("deferred function 2")
+	defer fmt.Println("deferred function 3")
+	i++
+	fmt.Println("The current value of i is", i)
+}
