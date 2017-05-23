3ab5e4b9dfc46ca4df9ea9edd2703b84fe8aee2c
Shiju Varghese <shiju.varghese@outlook.com>
2017-05-23 11:26:20 +0530

    Examples for control structures

--- examples/basics/control/if.go
+++ examples/basics/control/if.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 )
 
 func main() {
-	input := flag.Int("input", 0, "an integer value")
-	flag.Parse()
-	if *input%2 == 0 {
+	isEven(8)
+}
+func isEven(input int) {
+	if input%2 == 0 {
 		fmt.Println("Even number")
 	} else {
 		fmt.Println("Odd number")
