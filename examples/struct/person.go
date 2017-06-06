17d2dbf687bd482ee2d68143743c1d7af2f3b0d5
Shiju Varghese <shiju.varghese@outlook.com>
2017-06-06 05:58:16 +0530

    Modifications on struct

---
+++ examples/struct/person.go
@@ -0,0 +1,37 @@
+// Person struct with methods of value receiver
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	FirstName, LastName string
+	Dob                 time.Time
+	Email, Location     string
+}
+
+//A person method with pointer receiver
+func (p Person) PrintName() {
+	fmt.Printf("\n%s %s\n", p.FirstName, p.LastName)
+}
+
+//A person method with pointer receiver
+func (p Person) PrintDetails() {
+	fmt.Printf("[Date of Birth: %s, Email: %s, Location: %s ]\n", p.Dob.String(), p.Email, p.Location)
+}
+
+func main() {
+	p := Person{
+		"Shiju",
+		"Varghese",
+		time.Date(1979, time.February, 17, 0, 0, 0, 0, time.UTC),
+		"shiju@email.com",
+		"Kochi",
+	}
+	p.PrintName()
+	p.PrintDetails()
+
+}
