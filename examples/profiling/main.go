5ba4e6c01b3f1c6d99d7ff110dbc31ad2ec22817
Shiju Varghese <shiju.varghese@outlook.com>
2017-09-19 22:06:23 +0530

    minor modification

--- examples/profiling/main.go
+++ examples/profiling/main.go
@@ -14,6 +14,7 @@ func attachProfiler(router *mux.Router) {
 	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
