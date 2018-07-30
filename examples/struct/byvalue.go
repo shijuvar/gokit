c5590d144efc707bb2cf8e46b64b55e34f3ae102
shijuvar <gophermonk@gmail.com>
2018-07-30 09:50:56 +0530

    Minor modifications on struct examples

--- examples/struct/byvalue.go
+++ examples/struct/byvalue.go
@@ -13,19 +13,20 @@ type Person struct {
 	Email, Location     string
 }
 
-//A person method with pointer receiver
+//A person method with value receiver
 func (p Person) PrintName() {
 	fmt.Printf("\n%s %s\n", p.FirstName, p.LastName)
 }
 
-//A person method with pointer receiver
+//A person method with value receiver
 func (p Person) PrintDetails() {
 	fmt.Printf("[Date of Birth: %s, Email: %s, Location: %s ]\n", p.Dob.String(), p.Email, p.Location)
 }
 
-//A person method with pointer receiver
+//A person method with value receiver
 func (p Person) ChangeLocation(newLocation string) {
 	p.Location = newLocation
+
 }
 func main() {
 	p := Person{
---
+++ examples/struct/student.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Mark struct {
+	english int64
+	maths   int64
+	science int64
+	social  int64
+}
+
+type Student struct {
+	id   string
+	name string
+	Mark
+}
+
+func (s *Student) setName(name string) {
+	s.name = name
+}
+
+func (s *Student) setId(id string) {
+	s.id = id
+}
+
+func (s *Student) setMark(marks Mark) {
+	s.Mark = marks
+}
+
+func (s *Student) getStudentMark() {
+	fmt.Printf("English :: %d\n", s.english)
+	fmt.Printf("Maths :: %d\n", s.maths)
+	fmt.Printf("Science :: %d\n", s.science)
+	fmt.Printf("Social :: %d\n", s.social)
+}
+
+func main() {
+	m := Mark{
+		english: 90,
+		maths:   80,
+		science: 70,
+		social:  65,
+	}
+
+	s := &Student{
+		id:   "#1242",
+		name: "phani",
+		Mark: m,
+	}
+
+	m1 := m
+	m1.english = 85
+
+	s.setMark(m1) // mutate the Mark value
+	s.getStudentMark()
+}
