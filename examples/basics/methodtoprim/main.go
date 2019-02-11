b7cce8e3ded1e3d2db03b1e0acdb9171246629dd
shijuvar <gophermonk@gmail.com>
2019-02-11 19:39:30 +0530

    added an example on method to primitive types

---
+++ examples/basics/methodtoprim/main.go
@@ -0,0 +1,18 @@
+// Add methods to primitive types
+package main
+
+import "fmt"
+
+type Number int
+
+func (n Number) Positive() bool {
+	return n > 0
+}
+
+func main() {
+	num1 := Number(100)
+	num2 := Number(-50)
+	fmt.Printf("%d is positive: %t\n", num1, num1.Positive())
+	fmt.Printf("%d is positive: %t\n", num2, num2.Positive())
+
+}
