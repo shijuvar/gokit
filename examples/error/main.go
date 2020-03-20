2103bc7dc997e01e1bbfbf91bb0ef1b1eccd601b
shijuvar <gophermonk@gmail.com>
2020-03-20 10:13:50 +0530

    Modified error example

--- examples/error/main.go
+++ examples/error/main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
-
-	"github.com/pkg/errors"
 )
 
 // ReadFile read the given file
 func ReadFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to open file")
+		// Errorf formats according to a format specifier
+		// and returns the string as a value that satisfies error.
+		// If the format specifier includes a %w verb with
+		// an error operand, the returned error will implement
+		// an Unwrap method returning the operand.
+		return nil, fmt.Errorf("Failed to open file: %w", err)
 	}
 	defer f.Close()
 
 	buf, err := ioutil.ReadAll(f)
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to read the file")
+		return nil, errors.New("Failed to read the file")
 	}
 	return buf, nil
 }
 
 func main() {
-	data, err := ReadFile("data.txt")
+	data, err := ReadFile("data1.txt")
 	if err != nil {
 		fmt.Printf("%+v\n", err)
+		werror := errors.Unwrap(err)
+		if werror != nil {
+			fmt.Printf("Wrap: %s\n", werror)
+		}
 		os.Exit(1)
 	}
 	fmt.Print(string(data))
