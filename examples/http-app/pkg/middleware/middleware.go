dacc7663470d21ebc5e1b94d43a4068a2e432510
shijuvar <gophermonk@gmail.com>
2020-05-15 10:53:23 +0530

    Minor modifications

--- examples/http-app/pkg/middleware/middleware.go
+++ examples/http-app/pkg/middleware/middleware.go
@@ -5,8 +5,8 @@ import "net/http"
 type Middleware func(http.Handler) http.Handler
 
 func Apply(h http.Handler, mw ...Middleware) http.Handler {
-	for _, adapter := range mw {
-		h = adapter(h)
+	for _, middleware := range mw {
+		h = middleware(h)
 	}
 	return h
 }
