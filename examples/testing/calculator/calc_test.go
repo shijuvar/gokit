32f3b5bbfdf4c305854b80acb9bc419bb6107150
shijuvar <gophermonk@gmail.com>
2022-02-16 10:28:45 +0530

    Modifications in test examples

--- examples/testing/calculator/calc_test.go
+++ examples/testing/calculator/calc_test.go
@@ -15,9 +15,10 @@ type in struct {
 
 // Test organises values for tests
 type Test struct {
-	name string
-	in   in
-	out  float64
+	name        string
+	in          in
+	out         float64
+	shouldError bool
 }
 
 // TestCalcAdd tests the behavior of Calc with Add operation
@@ -40,12 +41,18 @@ func TestCalcAdd(t *testing.T) {
 		},
 	}
 	c := &Calculator{}
+	var result float64
 	for _, test := range tests {
+
+		result = c.Do(test.in.input, test.in.op)
+		//if test.out != result {
+		//	t.Errorf(fmt.Sprintf("%s: %v", test.name, test.in))
+		//}
 		// Assertion with assert package
 		assert.Equal(
 			t,
 			test.out,
-			c.Do(test.in.input, test.in.op),
+			result,
 			fmt.Sprintf("%s: %v", test.name, test.in),
 		)
 	}
@@ -70,7 +77,7 @@ func TestCalcMultipleOps(t *testing.T) {
 			out:  20,
 		},
 		Test{
-			name: "third input for subtraction",
+			name: "fourth input for subtraction",
 			in:   in{input: 4, op: "/"},
 			out:  5,
 		},
@@ -92,22 +99,22 @@ func TestCalcMultipleOps(t *testing.T) {
 func TestCalcMultipleOpsWithSubTest(t *testing.T) {
 	var tests = []Test{
 		Test{
-			name: "input for addition",
+			name: "first input for addition",
 			in:   in{input: 10, op: "+"},
 			out:  10,
 		},
 		Test{
-			name: "input for multiplication",
+			name: "second input for multiplication",
 			in:   in{input: 5, op: "*"},
 			out:  50,
 		},
 		Test{
-			name: "input for subtraction",
+			name: "third input for subtraction",
 			in:   in{input: 30, op: "-"},
 			out:  20,
 		},
 		Test{
-			name: "third input for subtraction",
+			name: "fourth input for subtraction",
 			in:   in{input: 4, op: "/"},
 			out:  5,
 		},
@@ -125,48 +132,48 @@ func TestCalcMultipleOpsWithSubTest(t *testing.T) {
 	}
 }
 
-// BenchmarkCalc benchmarks Calc
-func BenchmarkCalc(b *testing.B) {
-	test := Test{
-		in:  in{input: 5, op: "+"},
-		out: 5,
-	}
-	for i := 0; i < b.N; i++ {
-		c := &Calculator{}
-		c.Do(test.in.input, test.in.op)
-	}
-}
-
-// BenchmarkCalcMultipleOpsWithSubTest benchmarks with sub-level benchmark
-func BenchmarkCalcMultipleOpsWithSubTest(b *testing.B) {
-	var tests = []Test{
-		Test{
-			name: "input for addition",
-			in:   in{input: 10, op: "+"},
-			out:  10,
-		},
-		Test{
-			name: "input for multiplication",
-			in:   in{input: 5, op: "*"},
-			out:  50,
-		},
-		Test{
-			name: "input for subtraction",
-			in:   in{input: 30, op: "-"},
-			out:  20,
-		},
-		Test{
-			name: "third input for subtraction",
-			in:   in{input: 4, op: "/"},
-			out:  5,
-		},
-	}
-	for _, test := range tests {
-		b.Run(test.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				c := &Calculator{}
-				c.Do(test.in.input, test.in.op)
-			}
-		})
-	}
-}
+//// BenchmarkCalc benchmarks Calc
+//func BenchmarkCalc(b *testing.B) {
+//	test := Test{
+//		in:  in{input: 5, op: "+"},
+//		out: 5,
+//	}
+//	for i := 0; i < b.N; i++ {
+//		c := &Calculator{}
+//		c.Do(test.in.input, test.in.op)
+//	}
+//}
+//
+//// BenchmarkCalcMultipleOpsWithSubTest benchmarks with sub-level benchmark
+//func BenchmarkCalcMultipleOpsWithSubTest(b *testing.B) {
+//	var tests = []Test{
+//		Test{
+//			name: "input for addition",
+//			in:   in{input: 10, op: "+"},
+//			out:  10,
+//		},
+//		Test{
+//			name: "input for multiplication",
+//			in:   in{input: 5, op: "*"},
+//			out:  50,
+//		},
+//		Test{
+//			name: "input for subtraction",
+//			in:   in{input: 30, op: "-"},
+//			out:  20,
+//		},
+//		Test{
+//			name: "third input for subtraction",
+//			in:   in{input: 4, op: "/"},
+//			out:  5,
+//		},
+//	}
+//	for _, test := range tests {
+//		b.Run(test.name, func(b *testing.B) {
+//			for i := 0; i < b.N; i++ {
+//				c := &Calculator{}
+//				c.Do(test.in.input, test.in.op)
+//			}
+//		})
+//	}
+//}
