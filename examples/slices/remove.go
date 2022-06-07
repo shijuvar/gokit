b5604012d470b5cfa0425af112215c604ce07303
shijuvar <gophermonk@gmail.com>
2022-06-07 20:08:17 +0530

    Added example for removing elements from slice

--- examples/slices/remove.go
+++ examples/slices/remove.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type person struct {
+	fname, lname string
+}
+
 func main() {
 	fmt.Println(truncate([]int{10, 20, 30, 40, 50}, 2))
 	fmt.Println(truncateWithCopy([]int{10, 20, 30, 40, 50}, 2))
@@ -9,7 +13,13 @@ func main() {
 	fmt.Println(genericTruncate([]int{10, 20, 30, 40, 50}, 2))
 	fmt.Println(genericTruncate([]string{"A", "B", "C", "D", "E"}, 2))
 	fmt.Println(genericTruncate([]float64{10.65, 20.00, 30.50, 40.55, 50.75}, 2))
-
+	fmt.Println(genericTruncate([]person{
+		person{"Rahul", "Sharma"},
+		person{"Nivedita", "Dubey"},
+		person{"Raghu", "Kumar"},
+		person{"Gaurav", "Sharma"},
+		person{"Anil", "Menon"},
+	}, 2))
 }
 
 func truncate(sl []int, i int) []int {
@@ -27,12 +37,15 @@ func truncateWithCopy(sl []int, i int) []int {
 	return sl
 }
 func truncateWithAppend(sl []int, i int) []int {
+	// sl[:i] gets the slice with the data before the given index
+	// to which appending a slice, sl[i+1:]... which is the slice
+	// that takes data after the given index
 	sl = append(sl[:i], sl[i+1:]...)
 	return sl
 }
 
 type valuetype interface {
-	~int | ~int32 | ~int64 | ~float64 | ~string
+	~int | ~int32 | ~int64 | ~float64 | ~string | person
 }
 
 func genericTruncate[V valuetype](sl []V, i int) []V {
