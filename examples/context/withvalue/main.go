9bcbc0ca05007cd4d75f27b84da9e45ae5167215
Shiju Varghese <gophermonk@gmail.com>
2023-12-06 10:04:47 +0530

    modifications for context examples

--- examples/context/withvalue/main.go
+++ examples/context/withvalue/main.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 )
 
+type role struct {
+	user string
+	role string
+}
+
 func doSomething(ctx context.Context) {
 	if ctx.Value("authorization") != "my-auth-token" {
 		fmt.Println("Unauthorized from doSomething")
@@ -20,12 +25,20 @@ func doAnother(ctx context.Context) {
 	}
 	fmt.Println("Authorized from doAnother:", ctx.Value("user"))
 }
-
+func doAdmin(ctx context.Context) {
+	if ctx.Value("authorization") == "my-auth-token" {
+		fmt.Printf("Role:%+v", ctx.Value("role").(role))
+	}
+}
 func main() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "authorization", "my-auth-token")
 	ctx = context.WithValue(ctx, "user", "shijuvar")
-
 	doSomething(ctx)
 	doAnother(ctx)
+	ctx = context.WithValue(ctx, "role", role{
+		user: "shijuvar",
+		role: "admin",
+	})
+	doAdmin(ctx)
 }
